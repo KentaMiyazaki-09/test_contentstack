b73a9d966713286327ec74be9aab971c8050c07e
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-11-12 18:22:34 +0900

    記事一覧をマウント

--- app/components/lib/contentstack.ts
+++ app/components/lib/contentstack.ts
@@ -13,8 +13,14 @@ export const getPageData = async () => {
   return entries[0]; // Homeページ想定
 };
 
+export const PreviewStack = contentstack.Stack({
+  api_key: process.env.CONTENTSTACK_API_KEY!,
+  delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN!,
+  environment: process.env.CONTENTSTACK_ENVIRONMENT!,
+});
+
 export const getArticlesData = async () => {
-  const Query = Stack.ContentType("articles").Query();
+  const Query = PreviewStack.ContentType("articles").Query();
   const result = await Query.toJSON().find();
   const [entries] = result;
   return entries;
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,12 @@ export default async function Home() {
   const articles = await getArticlesData();
   console.log(articles);
 
+  type itemProps = {
+    uid: string;
+    title: string;
+    one_linear: string;
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -16,7 +22,14 @@ export default async function Home() {
           <Image src={banner.url} alt="" width={745} height={419} />
         )}
 
-        {}
+        <div>
+          {articles.map((item: itemProps) => (
+            <div key={item.uid}>
+              <h2>{item.title}</h2>
+              <p>{item.one_linear}</p>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
